b886bacfc389f1f88c673319a79715fe7358126b
hexinova <shafirnick@gmail.com>
2026-04-09 21:00:49 -0400

    ye

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MinesGame from "./Minesgame.jsx";
+
+function App() {
+  return (
+    <>
+      <main>
+        <MinesGame /> {/* 2. Use the component here */}
+      </main>
+    </>
+  );
+}
+
+export default App;
